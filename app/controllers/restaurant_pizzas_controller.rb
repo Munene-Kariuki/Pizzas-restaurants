6b14f418deecd3c5e9ad43456c39ed54b406587a
Denis <denismunene999@gmail.com>
2022-10-06 18:39:27 +0300

    add restaurant_pizza post route

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantPizzasController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record_response
+
+  def create 
+    restaurant_pizza = RestaurantPizza.create!(restaurant_pizza_params) 
+    render json: restaurant_pizza, status: :created 
+  end 
+
+  private 
+  def restaurant_pizza_params 
+    params.permit(:price, :restaurant_id, :pizza_id) 
+  end
+
+  def render_invalid_record_response(exception)
+    render json: {errors: exception.record.errors.full_messages}
+  end
+
 end
