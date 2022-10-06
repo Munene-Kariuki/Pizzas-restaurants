afbbf20613229db8f2ae289015f75ea38cd0aedc
Denis <denismunene999@gmail.com>
2022-10-06 18:08:41 +0300

    configure restaurant get routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,22 @@
 class RestaurantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_restaurant_not_found
+
+  def index 
+    render json: Restaurant.all
+  end 
+  
+  def show 
+    restaurant = find_restaurant 
+    render json: restaurant, include: :pizzas
+  end 
+
+  private 
+  def find_restaurant 
+    Restaurant.find(params[:id]) 
+  end
+
+  def render_restaurant_not_found 
+    render json: {error: "Restaurant not found"}, status: :not_found 
+  end
+
 end
